2abd9fd5ca5331b918eff55ac5c846697e525ac3
Monty West <monty.p.west@gmail.com>
2019-02-22 17:20:13 +0000

    added quick sort for ints groll:initial

--- src/main/scala/mw/ops/package.scala
+++ src/main/scala/mw/ops/package.scala
@@ -2,6 +2,11 @@ package mw
 
 package object ops {
 
-  def sortInts(ls: List[Int]): List[Int] = ???
+  def sortInts(ls: List[Int]): List[Int] = ls match {
+    case Nil => Nil
+    case _ :: Nil => ls
+    case x :: xs =>
+      sortInts(xs.filter(a => a < x)) ++ List(x) ++ sortInts(xs.filterNot(a => a < x))
+  }
 
 }
