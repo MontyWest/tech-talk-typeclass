dc5f2371fd0b3f6a110345bb10c8fc64d53767c0
Monty West <monty.p.west@gmail.com>
2019-02-22 17:50:11 +0000

    added Orderable instance for Person

--- src/main/scala/mw/domain/Person.scala
+++ src/main/scala/mw/domain/Person.scala
@@ -1,3 +1,14 @@
 package mw.domain
 
 final case class Person(age: Int, name: String)
+
+object Person {
+  implicit val personOrderable: Orderable[Person] = new Orderable[Person] {
+    /** *
+      * l < r : negative
+      * l = r : zero
+      * l > r : positive
+      */
+    override def compare(l: Person, r: Person): Int = l.age - r.age
+  }
+}
