8a89be9de7585f402855856a3f9b64fdf50d02d6
Monty West <monty.p.west@gmail.com>
2019-02-22 18:00:50 +0000

    added composable helpers

--- src/main/scala/mw/domain/Orderable.scala
+++ src/main/scala/mw/domain/Orderable.scala
@@ -16,4 +16,15 @@ object Orderable {
 
   implicit val intOrderable: Orderable[Int] = (l: Int, r: Int) => l - r
 
+  implicit val stringOrderable: Orderable[String] =
+    (l: String, r: String) =>
+      if (l == r) 0
+      else if (l < r) -1
+      else 1
+
+  def by[A, B](f: A => B)(implicit order: Orderable[B]): Orderable[A] =
+    (l: A, r: A) => order.compare(f(l), f(r))
+
+  def reverse[A](implicit order: Orderable[A]): Orderable[A] =
+    (l: A, r: A) => order.compare(r, l)
 }
