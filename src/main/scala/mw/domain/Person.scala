a3e87b13d24559e5ac4983eb0d265fe4090bceb4
montywest <monty.p.west@gmail.com>
2019-03-13 11:45:39 +0000

    added tuple sort

--- src/main/scala/mw/domain/Person.scala
+++ src/main/scala/mw/domain/Person.scala
@@ -2,9 +2,13 @@ package mw.domain
 
 import cats.Order
 import cats.instances.int._
+import cats.instances.string._
+import cats.instances.tuple._
 
 final case class Person(age: Int, name: String)
 
 object Person {
-  implicit val personOrderable: Order[Person] = Order.by(_.age)
+  implicit val personOrderable: Order[Person] = Order.by { person =>
+    (person.age, person.name)
+  }
 }
